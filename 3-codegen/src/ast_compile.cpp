bf97dd249e50f9aeeb47f479ebfe2ebc782103d9
DenizzG <denizgoy2003@gmail.com>
2024-02-25 21:00:35 +0000

    Completed Interpreter/Compiler 16/16

--- 3-codegen/src/ast_compile.cpp
+++ 3-codegen/src/ast_compile.cpp
@@ -32,10 +32,96 @@ void CompileRec(
         for(unsigned i=0; i<program->branches.size(); i++){
             CompileRec(destReg, program->branches[i]);
         }
-    
-    // TODO : handle the others
-        
-    }else{
+    }
+    else if(program->type== "Output"){  //can i output a deistantion register?
+        (CompileRec(destReg, program->branches[0])); //run program, put value of calculation into destReg
+        // We need to run the calucaltion and store the value in destReg
+        //The program is recusrive, so destReg will be different destinations, and will contain the value of the output
+        //so we just need to output it and return its value at the end of the code.
+        std::cout << "output " << destReg << std::endl;
+        //std::cout << "halt " << destReg << std::endl;   no need as already stored in destReg
+    }
+    else if(program->type =="Input"){
+        std::cout << "input " << destReg << std::endl;
+        //maybe return
+    }
+    else if(program->type == "LessThan"){ //not sure if i can stoe into srcReg and use it in compilation
+        std::string val1 = makeName("val1");
+        std::string val2 = makeName("val2");
+        CompileRec(val1, program->branches[0]);
+        CompileRec(val2, program->branches[1]);
+        std::cout << "lt " << destReg << " " <<  val1 << " " << val2 << std::endl;
+    }
+    else if(program->type == "Add"){
+        std::string val1 = makeName("val1");
+        std::string val2 = makeName("val2");
+        CompileRec(val1, program->branches[0]);
+        CompileRec(val2, program->branches[1]);
+        std::cout << "add " << destReg << " " <<  val1 << " " << val2 << std::endl;
+    }
+    else if(program->type == "Sub"){
+        std::string val1 = makeName("val1");
+        std::string val2 = makeName("val2");
+        CompileRec(val1, program->branches[0]);
+        CompileRec(val2, program->branches[1]);
+        std::cout << "sub " << destReg << " " <<  val1 << " " << val2 << std::endl;
+    }
+    else if(program->type == "Assign"){
+        std::string zero = makeName("zero");
+        std::cout<<"const "<<zero<<" 0"<<std::endl;
+        CompileRec(destReg, program->branches[0]);
+        std::cout << "add " << program->value << " " << destReg << " " << zero << std::endl;
+    }
+    else if(program->type == "If"){
+        std::string branchval = makeName("branchval");
+        CompileRec(branchval, program->branches[0]);       
+        std::string zero = makeName("zero");
+        std::cout << "const "<<zero<<" 0"<<std::endl;
+        std::string equalzero = makeName("equalzero");
+        std::string end = makeName("end");
+        std::cout << "beq " << branchval << " " << zero << " " << equalzero << std::endl;
+        CompileRec(destReg, program->branches[1]);
+        std::cout << "bne " << branchval << " " << zero << " " << end << std::endl;
+        std::cout << ":" << equalzero <<std::endl;
+        CompileRec(destReg, program->branches[2]);
+        std::cout << ":" << end <<std::endl;
+        /*
+        branchval = compilation 0
+        zero = 0
+        beq branchval zero equalzero 
+        destreg = compilation 1
+        bne branchval zero end
+        :equalzero
+        destReg = compilation 2
+        :end
+        */
+    }
+    else if(program->type == "While") { //check might not be correct with jumping to end and doing nothing
+        CompileRec(destReg, program->branches[0]);
+        std::string zero = makeName("zero");
+        std::cout << "const "<< zero <<" 0"<<std::endl;
+        std::string iszero = makeName("iszero");
+        std::string nonzero = makeName("nonzero");
+        std::cout << "beq " << destReg << " " << zero << " " << iszero << std::endl;
+        std::cout << ":" << nonzero <<std::endl;
+        CompileRec(destReg, program->branches[1]);
+        CompileRec(destReg, program->branches[0]);
+        std::cout << "bne " << destReg << " " << zero << " " << nonzero << std::endl;
+        std::cout << ":" << iszero <<std::endl;
+
+        /*
+        destReg = compilation 0
+        zero = 0
+        bne destReg zero nonzero
+        halt zero
+        :nonzero 
+        destReg = compilation 1;
+        destReg = compilation 0
+        bne destReg zero nonzero
+        halt zero
+        */
+    }
+    else{
         throw std::runtime_error("Unknown construct '"+program->type+"'");
     }
 }
--- 3-codegen/src/ast_interpret.cpp
+++ 3-codegen/src/ast_interpret.cpp
@@ -12,21 +12,82 @@ int32_t Interpret(
     if( regex_match(program->type, reNum) ){
         return std::atol(program->type.c_str());
         
+    
     // TODO : Check for things matching reId
-        
+    }else if( regex_match(program->type, reId) ){
+        //go to context bindings(dictionary of variable names and their values)
+        //search for name of varaible (program->type) and return the value of that variable
+        return context.bindings[program->type]; 
+
     }else if(program->type=="Param"){
-        unsigned index=atol(program->value.c_str());
-        auto value=context.params.at(index);
-        return value;
+        unsigned index=atol(program->value.c_str()); //stores integer
+        auto value=context.params.at(index); //context.params stores parameters ,gives the value at that param index
+        return value; //returns the value
         
     }else if(program->type=="Output"){
-        int32_t val=Interpret(context, program->branches.at(0));
-        std::cout<<val<<std::endl;
+        int32_t val=Interpret(context, program->branches.at(0)); //32 bit integer, interpret to get value and then output
+        std::cout << val << std::endl;
         return val;
         
-    // TODO: Handle other constructs
+    }else if(program->type == "Input"){
+        int32_t val;
+        std::cin >> val;
+        return val;
+    }
+    else if (program->type == "LessThan"){
+        int32_t a = Interpret(context, program->branches.at(0));
+        int32_t b = Interpret(context, program->branches.at(1));
+        if (a<b) return 1;
+        return 0;
+    }
+    else if (program->type == "Add"){
+        int32_t a = Interpret(context, program->branches.at(0));
+        int32_t b = Interpret(context, program->branches.at(1));
+        a +=b;
+        return a;
+    }
+    else if (program->type == "Sub"){
+        int32_t a = Interpret(context, program->branches.at(0));
+        int32_t b = Interpret(context, program->branches.at(1));
+        a = a - b;
+        return a;
+    }
+    else if(program->type == "Assign"){
+        int32_t n = Interpret(context, program->branches.at(0));
+        context.bindings[program->value] = n;
+        return n;
+    }  
+
+    // TODO: Handle other constructs (Seq, If, While)
+    else if(program->type == "Seq"){
+        int32_t n;
+        for(unsigned i_counter = 0; i_counter < program->branches.size(); i_counter++){
+            n = Interpret(context, program->branches.at(i_counter)); //interpret all instrctions until you have last instruction which is returned
+        }
+        return n;
+    }
+
+    else if(program->type == "If"){
+        int32_t n = Interpret(context, program->branches.at(0));
+        if (n == 0){
+            int32_t y = Interpret(context, program->branches.at(2));
+            return y;
+        }
+        else{
+            int32_t x = Interpret(context, program->branches.at(1));
+            return x;
+        }
+    }
+
+    else if(program->type == "While"){
+        while (Interpret(context, program->branches.at(0)) != 0){
+            int32_t n = Interpret(context, program->branches.at(1));
+        }
+        return 0;
+    }
+
         
-    }else{
+    else{
         throw std::runtime_error("Unknown construct '"+program->type+"'");
     }
 }
